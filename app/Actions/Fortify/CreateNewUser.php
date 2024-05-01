ea669ccef00cabfcc66606f81c6f3b14ac0dba27
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-01 20:22:49 +0900

    Add user creation and role assignment in CreateNewUser.php

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,11 +30,15 @@ class CreateNewUser implements CreatesNewUsers
             ],
             'password' => $this->passwordRules(),
         ])->validate();
-
-        return User::create([
+        //create dulu usernya
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+        //set user role
+        $user->assignRole('user');
+        //return user
+        return $user;
     }
 }
