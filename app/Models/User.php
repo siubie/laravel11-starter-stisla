958370fb5a290bff0ea0330f9df108448bc36e8a
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-02 06:47:24 +0900

    Add CustomEmailVerificationMessage notification and update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomEmailVerificationMessage;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -43,4 +44,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new CustomEmailVerificationMessage);
+    }
 }
